fdee8f52e86b32ef687c74ae3129a577b59751e2
09tarni <158708199+09tarni@users.noreply.github.com>
2024-09-01 13:26:41 +0530

    Create LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,25 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 42;
+        int num3 = 17;
+
+        int largest;
+
+        if (num1 >= num2) {
+            if (num1 >= num3) {
+                largest = num1;
+            } else {
+                largest = num3;
+            }
+        } else {
+            if (num2 >= num3) {
+                largest = num2;
+            } else {
+                largest = num3;
+            }
+        }
+
+        System.out.println("The largest number is: " + largest);
+    }
+}
